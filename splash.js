de7867cfa9fac5ce4b6d2acf60ec237b66165e9f
LaShelle Rauseo <lrauseo3@gmail.com>
2022-10-03 11:49:32 -0500

    created game play page with styling

--- splash.js
+++ splash.js
@@ -1,14 +1,8 @@
-//DOM 
+//DOM
 const button = document.querySelector(button)
 
-
 //FUNCTIONS
-
-
-
-
+const readyToPlay = () => {}
 
 //EVENT LISTENERS
-button.addEventListener("click", readyToPlay) =>{
-    
-}
+button.addEventListener('click', readyToPlay)
