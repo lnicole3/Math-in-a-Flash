7439b3b6a5473f0071ff36c4dc8ffe7e14a7e468
LaShelle Rauseo <lrauseo3@gmail.com>
2022-10-04 09:17:49 -0500

    completed evalAnswer/generateEquation function with submit button

--- add.js
+++ add.js
@@ -6,7 +6,8 @@ let userAnswer
 let equation = document.querySelector('card')
 let answer
 const button = document.getElementById('submit')
-
+let evalScore
+let score = 0
 //ARRAYS AND OBJECTS
 
 //EVENT LISTENERS
@@ -18,15 +19,21 @@ const generateEquation = () => {
   answer = parseInt(num1.innerText) + parseInt(num2.innerText)
 }
 
-const evalEquation = () => {
+const drawScore = () => {}
+
+//->input answer
+const evalAnswer = () => {
   if (answer === userAnswer) {
+    score = +10
   } else {
+    score = -10
   }
+  generateEquation()
 }
 
 button.addEventListener('click', () => {
   userAnswer = parseInt(document.getElementById('userAnswer').value)
-  evalEquation()
+  evalAnswer()
 })
 
 generateEquation()
